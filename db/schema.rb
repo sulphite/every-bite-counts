587ed965ca1ed71fc4397b68225ce75623403e98
Joel Savage <joel28169@gmail.com>
2023-02-27 16:42:19 +0000

    Fixed migration errors with db

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_150535) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_163206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
+    t.string "date_from"
+    t.string "date_to"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "donuts_id"
+    t.index ["donuts_id"], name: "index_bookings_on_donuts_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "donuts", force: :cascade do |t|
@@ -30,7 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_150535) do
     t.integer "wholeness", default: 100
     t.bigint "user_id"
     t.index ["user_id"], name: "index_donuts_on_user_id"
-
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -56,6 +60,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_150535) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "donuts", column: "donuts_id"
+  add_foreign_key "bookings", "users"
   add_foreign_key "donuts", "users"
   add_foreign_key "reviews", "bookings"
 end
