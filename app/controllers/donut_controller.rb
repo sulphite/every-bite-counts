98ab2bfb4451d7950b43641858295ad391cdb189
Nik Stojkov <nik.stojkov@gmail.com>
2023-02-27 15:33:51 +0000

    created donut show action

--- app/controllers/donut_controller.rb
+++ app/controllers/donut_controller.rb
@@ -2,4 +2,8 @@ class DonutController < ApplicationController
   def index
     @donuts = Donut.all
   end
+
+  def show
+    @donut = Donut.find
+  end
 end
--- app/models/donut.rb
+++ app/models/donut.rb
@@ -1,2 +1,3 @@
 class Donut < ApplicationRecord
+  belongs_to :user
 end
