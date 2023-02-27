9b46450484eedfcb389f1dc9558a9e2d6b7b39b6
Janett Gerrard <mikanchik@gmail.com>
2023-02-27 15:35:17 +0000

    added validations and relationships to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :donuts
+  has_many :bookings
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 2 }
 end
