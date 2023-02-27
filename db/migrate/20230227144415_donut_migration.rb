c7a83dcf652effc577633904af2ad85cfa4be0c4
Nik Stojkov <nik.stojkov@gmail.com>
2023-02-27 15:02:25 +0000

    donut migration v1 created, columns added

---
+++ db/migrate/20230227144415_donut_migration.rb
@@ -0,0 +1,9 @@
+class DonutMigration < ActiveRecord::Migration[7.0]
+  def change
+    add_column :donuts, :title, :string
+    add_column :donuts, :description, :text
+    add_column :donuts, :flavour, :string
+    add_column :donuts, :location, :string
+    add_column :donuts, :wholeness, :integer, default: 100
+  end
+end
