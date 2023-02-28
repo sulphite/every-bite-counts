24270fa41108333ce6fed8608ea30d337066b6e3
Nik Stojkov <nik.stojkov@gmail.com>
2023-02-28 11:31:58 +0000

    run migrations, no other changes

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_163206) do
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.string "date_from"
-    t.string "date_to"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "donuts_id"
     t.index ["donuts_id"], name: "index_bookings_on_donuts_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "donuts", force: :cascade do |t|
@@ -61,7 +57,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_163206) do
   end
 
   add_foreign_key "bookings", "donuts", column: "donuts_id"
-  add_foreign_key "bookings", "users"
   add_foreign_key "donuts", "users"
   add_foreign_key "reviews", "bookings"
 end
