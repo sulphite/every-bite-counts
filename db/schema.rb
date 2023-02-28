a189f3ffc15b51e7c102fdfddcc03fdffb9c0603
Nik Stojkov <nik.stojkov@gmail.com>
2023-02-28 15:11:32 +0000

    all sorted in 15 seconds by Lukas

--- db/schema.rb
+++ db/schema.rb
@@ -43,10 +43,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_142259) do
   end
 
   create_table "bookings", force: :cascade do |t|
+    t.string "date_from"
+    t.string "date_to"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "donut_id", null: false
     t.index ["donut_id"], name: "index_bookings_on_donut_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "donuts", force: :cascade do |t|
@@ -87,6 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_142259) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "donuts"
+  add_foreign_key "bookings", "users"
   add_foreign_key "donuts", "users"
   add_foreign_key "reviews", "bookings"
 end
