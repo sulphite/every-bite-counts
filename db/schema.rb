c3f5573a3927c61aa0f07248782803d77297164a
Janett Gerrard <mikanchik@gmail.com>
2023-02-28 17:05:32 +0000

    basic homepage done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_153821) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_154930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,8 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_153821) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.string "date_from"
-    t.string "date_to"
+    t.string "time_from"
+    t.string "time_to"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
