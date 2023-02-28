e2b9544cef9e223f587d0b9d93039c20f46fa442
Nik Stojkov <nik.stojkov@gmail.com>
2023-02-28 15:07:38 +0000

    seeing if schema changes take effect

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_142259) do
     t.datetime "updated_at", null: false
     t.bigint "donut_id", null: false
     t.index ["donut_id"], name: "index_bookings_on_donut_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "donuts", force: :cascade do |t|
@@ -88,7 +87,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_142259) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "donuts"
-  add_foreign_key "bookings", "users"
   add_foreign_key "donuts", "users"
   add_foreign_key "reviews", "bookings"
 end
