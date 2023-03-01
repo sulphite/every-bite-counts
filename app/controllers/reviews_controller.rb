40b05979e2dce371696b39348f6dfe68e396f257
Nik Stojkov <nik.stojkov@gmail.com>
2023-03-01 12:58:48 +0000

    added comments to controller as unsure of some things, still works fine though

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,8 +9,10 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.booking = @booking
     @user = current_user.id
+    # should @user be ... current_user.id ... or ... @booking.user_id
     if @review.save
       redirect_to root_path
+      # redirects to root for now, until user show page is created
     else
       render :new, status: :unprocessable_entity
     end
@@ -18,7 +20,7 @@ class ReviewsController < ApplicationController
 
   ### MUST ADD REVIEW TO USER,
 
-  
+
 
   private
 
