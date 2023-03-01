d86ee17f77f5616d8e7e8957df65245abd9633d1
Joel Savage <joel28169@gmail.com>
2023-03-01 12:09:53 +0000

    Created Users Donuts page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def show
+    @user = User.find(params[:id])
+    @donuts = @user.donuts
+    # raise
+  end
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
