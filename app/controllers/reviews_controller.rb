5e4e0efc6352912df3c5bb7d6c0ec3abc46ee218
Nik Stojkov <nik.stojkov@gmail.com>
2023-03-02 16:48:08 +0000

    edit functions added to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_booking, only: %i[new create]
+  before_action :set_booking, only: %i[new create update]
 
   def new
     @review = Review.new
@@ -11,16 +11,20 @@ class ReviewsController < ApplicationController
     @user = current_user.id
     # should @user be ... current_user.id ... or ... @booking.user_id
     if @review.save
-      redirect_to root_path
+      redirect_to bookings_path(@booking)
       # redirects to root for now, until user show page is created
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  ### MUST ADD REVIEW TO USER,
+  def edit
+  end
 
-
+  def update
+    @review.update(review_params)
+    redirect_to donut_path(@donut)
+  end
 
   private
 
